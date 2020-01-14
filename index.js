5790e1a81b548a03f971af69c0d86c3b9e3e6755
mogumogu-tatsugoro <mogumogu.tatsugoro@gmail.com>
2020-01-14 22:28:12 +0900

    要素の属性のバインディング例

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
-let app = new Vue({
+const app = new Vue({
   el: '#app',
   data: {
     message: 'Hello Vue!'
+  }
+});
+
+const app2 = new Vue({
+  el: '#app2',
+  data: {
+    message: 'You loaded this page on ' + new Date().toLocaleString()
   }
 });
