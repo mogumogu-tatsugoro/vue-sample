0331749433696e05341fb211785ff85456cdbdf7
mogumogu-tatsugoro <mogumogu.tatsugoro@gmail.com>
2020-01-15 22:44:04 +0900

    ライフサイクルフックの挙動確認コード追加

--- 02-instance/index.js
+++ 02-instance/index.js
@@ -42,3 +42,32 @@ const app3 = new Vue({
 console.log(app3.$el === document.getElementById('app3'));
 console.log(app3.$data === data3);
 // true
+
+
+const vm4 = new Vue({
+  el: '#vm4',
+  data: {
+    no: 0,
+    text: 'text'
+  },
+  methods: {
+    printNo: function() {
+      return this.no += 1
+    },
+    test: function() {
+      this.text = ''
+    },
+  },
+  created: function() {
+    console.log(`created is ${this.printNo()}`)
+    // created is 1
+  },
+  mounted: function() {
+    console.log(`mounted is ${this.printNo()}`)
+    // mounted is 2
+  },
+  updated: function() {
+    console.log(`updated is ${this.printNo()}`)
+    // updated is 3
+  },
+});
