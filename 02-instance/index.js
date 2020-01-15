720da36b98aeb8f9fa9ca4c608905966ec1fb275
mogumogu-tatsugoro <mogumogu.tatsugoro@gmail.com>
2020-01-15 22:12:43 +0900

    Object.freeze すると更新されない例を追加

--- 02-instance/index.js
+++ 02-instance/index.js
@@ -17,3 +17,17 @@ console.log( vm.test === data.test );
 app.testes = 'test3';
 console.log(`app.testes => ${app.testes}`);
 console.log(`data.testes === typeof undefined => ${typeof data.testes === 'undefined'}`);
+
+let data2 = { a: 1 };
+
+Object.freeze(data2);
+
+const app2 =  new Vue({
+  el: '#app2',
+  data: data2,
+  methods: {
+    changeData: function() {
+      this.a += this.a
+    }
+  }
+});
