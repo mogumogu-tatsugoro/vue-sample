14932c004de2680f226bcbc7d794d409472e1d95
mogumogu-tatsugoro <mogumogu.tatsugoro@gmail.com>
2020-01-15 22:00:53 +0900

    インスタンス生成後に追加されたプロパティは双方向バインディングの対象とならない例を追加

--- 02-instance/index.js
+++ 02-instance/index.js
@@ -13,3 +13,7 @@ console.log( vm.test === data.test );
 data.test = 'test2';
 console.log( data.test );
 console.log( vm.test === data.test );
+
+app.testes = 'test3';
+console.log(`app.testes => ${app.testes}`);
+console.log(`data.testes === typeof undefined => ${typeof data.testes === 'undefined'}`);
